a7476538cada7eab0e36afce6b3b597535bec594
Rens-Surface <rvanvendeloo@gressive.nl>
2023-02-21 15:23:48 +0100

    Working code for W5

--- TINLES01_W5/main.c
+++ TINLES01_W5/main.c
@@ -10,37 +10,46 @@ uint8_t transfer_complete = 0;
 
 void pin_setup()
 {
-    P1->SEL0 &= 0x01;
-    P1->SEL1 &= 0x00;
+    //P1->SEL0 &= 0x01;
+    //P1->SEL1 &= 0x00;
+    P1->SEL0 |= 0xC0;
+    P1->SEL1 &= ~0xC0;
 }
 
 void twi_master_setup()
 {
     __disable_irq();
     EUSCI_B0->CTLW0 = 0x01;     //0b 0000 0000 0000 0001
-    EUSCI_B0->CTLW0 = 0x0F41;   //0b 0000 1111 0100 0001
+    //EUSCI_B0->CTLW0 = 0x0F41;   //0b 0000 1111 0100 0001
+    EUSCI_B0->CTLW0 = 0x0F51;   //0b 0000 1111 0100 0001
     EUSCI_B0->CTLW1 = 0x08;     //0b 0000 0000 0000 1000
     EUSCI_B0->TBCNT = 0x01;     //0b 0000 0000 0000 0001
     EUSCI_B0->BRW = 0x40;       //0b 0000 0000 0100 0000
     EUSCI_B0->CTLW0 &=~0x01;    //0b 0000 0000 0000 0001
-    EUSCI_B0->IE |= 0x8;        //0b 0000 0000 0000 1000
-    NVIC_SetPriority(EUSCIB0_IRQn, 3);
-    NVIC_EnableIRQ(EUSCIB0_IRQn);
+    //EUSCI_B0->IE |= 0x8;        //0b 0000 0000 0000 1000
+    EUSCI_B0->IE = 0x0003;
+    //NVIC_SetPriority(EUSCIB0_IRQn, 3);
+    //NVIC_EnableIRQ(EUSCIB0_IRQn);
+    NVIC->IP[20] = 0x60;            // priority
+    NVIC->ISER[0] |= 0x00100000;    // enable
     __enable_irq();
 
 }
 void i2c_B0_transmit()
 {
     EUSCI_B0->I2CSA = ADDRESS;
-    EUSCI_B0->CTLW0 |= 0xF;     //0b 0000 0000 0001 0000
+    //EUSCI_B0->CTLW0 |= 0xF;     //0b 0000 0000 0001 0000
+    EUSCI_B0->CTLW0 |= 0x0010;
     EUSCI_B0->CTLW0 |= 0x2;     //0b 0000 0000 0000 0010
+    transfer_complete = 0;  //new, check your workshop manual
 }
-void i2c_B0_recieve()
+void i2c_B0_receive()
 {
     EUSCI_B0->I2CSA = ADDRESS;
-    EUSCI_B0->CTLW0 &=~0xF;     //0b 0000 0000 0001 0000
+    //EUSCI_B0->CTLW0 &=~0xF;     //0b 0000 0000 0001 0000
+    EUSCI_B0->CTLW0 &=~0x0010;
     EUSCI_B0->CTLW0 |= 0x2;     //0b 0000 0000 0000 0010
-    transfercomplete = 0;       //0b 0000 0000 0000 0000
+    transfer_complete = 0;       //0b 0000 0000 0000 0000
 }
 void EUSCIB0_IRQHandler()
 {
@@ -51,7 +60,8 @@ void EUSCIB0_IRQHandler()
         EUSCI_B0->IFG &= ~BIT1; // clear flag
         if (!transfer_complete) {
             EUSCI_B0->TXBUF = data; // load data
-            transfercomplete = 1;
+            //transfercomplete = 1;
+            transfer_complete = 1;
         } else {
             EUSCI_B0->CTLW0 |= UCTXSTP; // issue stop
             EUSCI_B0->IFG &= ~BIT1; // clear flag
@@ -63,7 +73,8 @@ void EUSCIB0_IRQHandler()
         EUSCI_B0->IFG &= ~BIT0; // clear flag
         if (!transfer_complete) {
             data = EUSCI_B0->RXBUF; // read data
-            transfercomplete = 1; // stop reading
+            //transfercomplete = 1; // stop reading
+            transfer_complete = 1; // stop reading
         }
     }
 }
@@ -72,15 +83,18 @@ void EUSCIB0_IRQHandler()
  */
 void main(void)
 {
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
-	pin_setup();
-	twi_master_setup();
-	i2c_B0_transmit();
-	delay_ms(1000);
-	while(1){
-	    i2c_B0_receive();
-	    printf(data);
-	}
+    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
+    pin_setup();
+    twi_master_setup();
+    data = 0x00;    //new
+    i2c_B0_transmit();
+    delay_ms(1000);
+    while(1){
+        i2c_B0_receive();
+        delay_ms(1000); //new
+        int8_t temp = (int8_t)data; // new
+        printf("Temperature is: %d\n",temp);
+    }
 }
 
 
